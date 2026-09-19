c23f35863a016487ac5b454a98f62df8a500f5a9
Ayanfeoluwa Akindele <akindeleayanfeoluwa@gmail.com>
2026-09-19 08:27:25 +0100

    designed hero section

--- src/components/defaults/Hero.tsx
+++ src/components/defaults/Hero.tsx
@@ -1,14 +1,64 @@
-import React from 'react'
-import TopNav from './TopNav';
+import React from "react";
+import TopNav from "./TopNav";
+import { HeroImgAttachmentOne, HeroImgAttachmentTwo } from "../icons";
 
 const Hero = () => {
   return (
-    <div className="bg-[url('/hero.png')] bg-cover bg-center h-screen">
-        <div className="flex justify-center items-center mt-6">
-            <TopNav />
-        </div>
-    </div>
-  )
-}
+    <div className="bg-[url('/hero.png')] bg-cover bg-center min-h-screen lg:pb-20 pb-10">
+      <div className="flex justify-center items-center mt-6">
+        <TopNav />
+      </div>
 
-export default Hero
+      <div className="flex w-full max-w-[684px] flex-col items-center justify-center px-4 text-center mx-auto lg:mt-20 mt-10">
+        <h2 className="lg:text-[60px] text-[48px] font-semibold">
+          Ready to <span className="text-[#813993] kaushan">Own Your</span>{" "}
+          First Piece of <span className="text-[#FCCB07] kaushan">Land?</span>
+        </h2>
+        <p className="lg:text-[22px] mt-3 font-semibold">
+          Find the right land, secure your ownership, and take the first step
+          toward building something that lasts.
+        </p>
+        <div className="flex items-center space-x-3 mt-4">
+          <img src="/people.png" alt="people" />
+          <p className="lg:text-[14px] text-[12px] font-semibold">
+            200+ people and maybe you
+          </p>
+        </div>
+
+        <div className="flex lg:flex-row flex-col lg:space-x-3 lg:space-y-0 space-y-4 mt-4">
+          <button className="bg-[#FCCB07] py-2 px-6 rounded-full text-[14px] font-semibold hover:bg-[#FCCB07]/90">
+            Explore Our Listing {"->"}
+          </button>
+          <button className="text-[#813993] bg-white py-2 px-6 rounded-full text-[14px] font-semibold hover:bg-[#813993]/90 hover:text-white">
+            About Openage Group
+          </button>
+        </div>
+
+        <div className="my-6 flex lg:gap-10 gap-4 flex-row">
+          <div className="relative">
+            <img
+              src="/hero-img-2.png"
+              alt="A completed residential development"
+              className="lg:w-[324px] lg:h-[358px] w-[174px] h-[280px]"
+            />
+            <div className="absolute hidden lg:block right-10 top-0 z-10">
+              <HeroImgAttachmentOne />
+            </div>
+          </div>
+          <div className="relative">
+            <img
+              src="/hero-img-1.png"
+              alt="A residential estate property"
+              className="lg:w-[324px] lg:h-[358px] w-[174px] h-[280px]"
+            />
+            <div className="absolute hidden lg:block bottom-0 top-[20vh] left-0 z-10">
+              <HeroImgAttachmentTwo />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
